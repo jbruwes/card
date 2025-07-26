119a23a8023048b564841415c66ca14e1f48526f
Jerry Bruwes <jbruwes@icloud.com>
2025-07-26 18:04:05 +0200

    test

--- pages/313eda2d-40ea-428d-85d0-26396ad5f657.vue
+++ pages/313eda2d-40ea-428d-85d0-26396ad5f657.vue
@@ -32,6 +32,7 @@ import { TresCanvas } from "@tresjs/core";
 import { EffectComposer, Glitch, SMAA } from '@tresjs/post-processing';
 import { shallowRef, provide, useTemplateRef, watch } from "vue";
 import gsap from "gsap";
+import { retrieveLaunchParams } from "@telegram-apps/sdk";
 
 const show = shallowRef(false),
   hide = shallowRef(true),
@@ -46,6 +47,20 @@ watch(show, () => {
     }
   });
 });
+
+
+try {
+  const { initDataRaw } = retrieveLaunchParams();
+  fetch("http://localhost:3000", {
+    method: "GET",
+    headers: {
+      Authorization: `tma ${initDataRaw}`
+    },
+  });
+} catch(err) {
+  console.log(err);
+ };
+
 
 </script>
 
