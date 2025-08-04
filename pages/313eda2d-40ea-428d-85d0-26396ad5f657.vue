b963e1631ab024d3747f37ab1f95d2886f3a7825
Jerry Bruwes <jbruwes@icloud.com>
2025-08-04 21:21:22 +0200

    modified:   pages/313eda2d-40ea-428d-85d0-26396ad5f657.vue

--- pages/313eda2d-40ea-428d-85d0-26396ad5f657.vue
+++ pages/313eda2d-40ea-428d-85d0-26396ad5f657.vue
@@ -24,9 +24,11 @@
       enter-to-class="animate-none">
       <el-button v-show="show[2] && hasFinishLoading" tag="a" size="large" href="https://bryusova.ru" target="_blank"
         rel="noopener noreferrer" class="mx-auto animate-pulse shadow-xl">
-        <div class="i-streamline:cursor-click-solid mr-2"></div>ПОМОГУ
-        РАЗОБРАТЬСЯ
-      </el-button>
+        <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
+          <path fill="currentColor" fill-rule="evenodd"
+            d="M3.658.162a.75.75 0 0 1 .918.53l.449 1.673a.75.75 0 1 1-1.45.389L3.129 1.08a.75.75 0 0 1 .53-.919M.072 3.755a.75.75 0 0 0 .53.918l1.673.448a.75.75 0 1 0 .388-1.448L.99 3.224a.75.75 0 0 0-.918.53m4.4 1.965a1 1 0 0 1 1.264-1.265l7.12 2.375c.92.307.908 1.612-.018 1.902l-2.026.637l2.7 2.7a1 1 0 0 1-1.415 1.414l-2.706-2.707l-.642 2.046c-.29.925-1.596.937-1.903.017zM2.066 8.755a.75.75 0 1 1-1.061-1.06L2.231 6.47a.75.75 0 0 1 1.06 1.06zm6.46-6.591a.75.75 0 0 0-1.06-1.061L6.24 2.328a.75.75 0 0 0 1.061 1.06z"
+            clip-rule="evenodd" />
+        </svg>ПОМОГУ РАЗОБРАТЬСЯ</el-button>
     </Transition>
     <el-popover placement="bottom" title="Что такое МАК-карты" width="80%" trigger="click" effect="dark"
       popper-style="word-break: normal;" :content>
@@ -37,9 +39,11 @@
             leave-active-class="animate__animated animate__fadeOutUp animate__slower" leave-from-class="animate-none"
             leave-to-class="animate-none">
             <el-button v-if="!show[0] && hasFinishLoading" color="DarkMagenta" size="large" dark>
-              <div class="i-streamline:help-question-1-solid mr-2"></div>УЗНАЙ О МАК
-              КАРТАХ
-            </el-button>
+              <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
+                <path fill="currentColor" fill-rule="evenodd"
+                  d="M14 7A7 7 0 1 1 0 7a7 7 0 0 1 14 0M5.75 5.25A1.25 1.25 0 1 1 7 6.5a.75.75 0 0 0-.75.75v.646a.75.75 0 1 0 1.5 0a2.751 2.751 0 1 0-3.5-2.646a.75.75 0 0 0 1.5 0M8 10.5a1 1 0 1 1-2 0a1 1 0 0 1 2 0"
+                  clip-rule="evenodd" />
+              </svg>УЗНАЙ О МАК КАРТАХ</el-button>
           </Transition>
         </div>
       </template>
@@ -50,9 +54,11 @@
         leave-from-class="animate-none" leave-to-class="animate-none">
         <el-button v-if="!show[0] && hasFinishLoading" class="shadow-xl shadow-cyan-500/50 animate-pulse" size="large"
           color="DarkMagenta" dark @click="() => { if (card) show[0] = true }">
-          <div class="i-streamline:eye-optic-remix mr-2"></div>НАЖМИ И УЗРИ СВОЮ КАРТУ
-          ДНЯ
-        </el-button>
+          <svg class="mr-2" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
+            <path fill="currentColor" fill-rule="evenodd"
+              d="M4.317 1.001a.625.625 0 0 1 .784.408l.485 1.533c.44-.104.912-.162 1.416-.162s.975.058 1.415.161l.481-1.531a.625.625 0 1 1 1.193.375l-.493 1.567a7 7 0 0 1 2.037 1.472l1.298-1.3a.625.625 0 0 1 .885.883l-1.375 1.377c.291.4.518.778.686 1.088a8 8 0 0 1 .352.748a2 2 0 0 1 .04.12l.01.044a.6.6 0 0 1 0 .239a1 1 0 0 1-.025.093l-.025.072q-.029.073-.075.181a8 8 0 0 1-.277.566a9.4 9.4 0 0 1-1.134 1.654c-1.027 1.197-2.664 2.439-4.993 2.439s-3.966-1.242-4.994-2.439A9.4 9.4 0 0 1 .875 8.935a8 8 0 0 1-.352-.747a2 2 0 0 1-.04-.12l-.01-.045c-.002-.013-.011-.06-.011-.12s.009-.106.011-.118a1 1 0 0 1 .025-.093q.011-.037.025-.072q.027-.074.074-.181c.063-.144.155-.339.278-.567c.17-.317.404-.704.704-1.112L.19 4.414a.625.625 0 1 1 .87-.898l1.329 1.288a7 7 0 0 1 2.016-1.451l-.496-1.567A.625.625 0 0 1 4.317 1M1.976 8.343a7 7 0 0 1-.218-.44a8.2 8.2 0 0 1 1.198-1.87C3.845 4.999 5.165 4.03 7.002 4.03s3.157.97 4.045 2.003a8.2 8.2 0 0 1 1.198 1.87c-.05.111-.121.262-.217.44a8.2 8.2 0 0 1-.98 1.432c-.889 1.034-2.21 2.003-4.046 2.003s-3.157-.97-4.046-2.003a8.2 8.2 0 0 1-.98-1.432m2.76-.47c.01-1.244 1.036-2.24 2.28-2.24s2.256.996 2.248 2.24c-.009 1.242-1.036 2.238-2.28 2.238s-2.256-.996-2.247-2.239"
+              clip-rule="evenodd" />
+          </svg>НАЖМИ И УЗРИ СВОЮ КАРТУ ДНЯ</el-button>
       </Transition>
     </div>
   </div>
@@ -78,7 +84,7 @@ import bridge from "@vkontakte/vk-bridge";
 
 const isVKMA = () => {
   return new Promise((resolve) => {
-    const timeoutId = setTimeout(() => resolve(false), 100);    
+    const timeoutId = setTimeout(() => resolve(false), 100);
     bridge.send("VKWebAppInit")
       .then(() => {
         clearTimeout(timeoutId);
@@ -90,7 +96,7 @@ const isVKMA = () => {
       });
   });
 },
-show = reactive(new Array(3).fill(false)),
+  show = reactive(new Array(3).fill(false)),
   title = "Карта дня 🙋‍♀️",
   message = "возвращайся завтра за новой картой",
   content = "Метафорические ассоциативные карты (МАК) — профессиональный инструмент психолога, который помогает «разговорить» Ваше подсознание. Потому что именно в подсознании и находятся ответы на все наши вопросы! Через интерпретацию изображений Вы получаете доступ к тому, что остаётся за пределами сознательного контроля.",
